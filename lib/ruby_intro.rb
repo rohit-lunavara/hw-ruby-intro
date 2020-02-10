b1d74cdc4220257db14786a40d02f1ddfd787ef3
rohit-lunavara <rohit.lunavara@gmail.com>
2020-02-10 17:30:44 -0500

    Done with the assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,96 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  res = 0
+  arr.each {
+    |x|
+    res += x
+  }
+  return res
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size <= 1
+    return arr[0] || 0
+  end
+  num1 = arr.max
+  arr.delete_at arr.index num1
+  num2 = arr.max
+  return num1 + num2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.size <= 1
+    return false
+  end
+  pairs = {}
+  arr.each {
+    |x|
+    pairs[x] = n-x
+  }
+  puts pairs
+  pairs.each {
+    |k, v|
+    if pairs.key? v
+      if pairs[v] == pairs[k]
+        return false
+      else
+        return true
+      end
+    end
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  ns = s.downcase
+  consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+  consonants.each {
+    |x|
+    if ns[0] == x
+      return true
+    end
+  }
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  begin
+    num = Integer(s, 2)
+  rescue => exception
+    return false
+  end
+  return num%4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def initialize(isbn, price)
+    if isbn == ''
+      raise ArgumentError
+    else
+      @isbn = isbn
+    end
+
+    if price <= 0 
+      raise ArgumentError
+    else
+      @price = price
+    end
+  end
+
+  public
+  def price_as_string
+    return "$%0.2f" %self.price
+  end
 end
